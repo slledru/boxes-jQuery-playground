dc043e89156e5d46680126018aa44a81d37d9cb5
slledru <slledru@msn.com>
2018-02-22 18:56:16 -0700

    selectors, css, attributes

--- boxes.js
+++ boxes.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+$(document).ready(() => {
+  let secretBox = $('#secretBox');
+  let h1 = $('<h1>secret box!</h1>');
+  secretBox.append(h1);
+  let row1Children = $('#row1').children();
+  row1Children.addClass('boxType3');
+  let row4LastChild = $('#row4').children().last();
+  row4LastChild.css('display', 'none');
+  $('.boxType1').css('background-color', 'white');
+  let row2FirstChild = $('#row2').children().first();
+  row2FirstChild.attr('class', '');
+  let allDivs = $('#container div').not('.row').not('#secretBox');
+
+  for (let i = 0; i < allDivs.length; i++) {
+    console.log(allDivs[i]);
+    $(allDivs[i]).width('2px');
+  }
+});
